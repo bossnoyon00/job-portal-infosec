de7eb4cf0029ab6b7cf48e887814a384303abe3d
bossnoyon00 <bossnoyon584@gmail.com>
2023-07-03 17:23:48 +0600

    firebase key enabled

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,14 +5,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_APIKEY,
-    authDomain: import.meta.env.VITE_AUTHDOMAIN,
-    projectId: import.meta.env.VITE_PROJECTID,
-    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-    appId: import.meta.env.VITE_APPID
+    apiKey: "AIzaSyASWuxbO46wJ-7_qrsny7IB8Fx3lmogcQE",
+    authDomain: "job-portal-a7f71.firebaseapp.com",
+    projectId: "job-portal-a7f71",
+    storageBucket: "job-portal-a7f71.appspot.com",
+    messagingSenderId: "259345243765",
+    appId: "1:259345243765:web:0c35dbfc986d51fd31a23c"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export default app;
